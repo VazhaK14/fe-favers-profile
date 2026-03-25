1b1e8783b8a48b48735262f8ed1077b729f73bbb
VazhaK14 <khayrivazhasocial@gmail.com>
2026-03-25 20:21:06 +0700

    init api & auth module

--- app/routes/Home/action.ts
+++ app/routes/Home/action.ts
@@ -1,3 +1,5 @@
-export async function action() {
+import type { ActionFunctionArgs } from "react-router";
+
+export async function action({ request }: ActionFunctionArgs) {
   return null;
 }
---
+++ src/modules/AuthModule/index.tsx
@@ -0,0 +1,3 @@
+export default function AuthModule() {
+  return <p>Test</p>;
+}
