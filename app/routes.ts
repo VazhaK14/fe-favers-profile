6b66ea2879d34f0a5e21481eb04bf1f3c003a7fb
ariefsolomon <ariefsolomon8@gmail.com>
2026-03-26 07:56:13 +0700

    feat: setup better-auth with google provider

--- app/routes.ts
+++ app/routes.ts
@@ -3,4 +3,5 @@ import { route } from "@react-router/dev/routes";
 export default [
   index("routes/Home/page.tsx"),
   route("/auth", "routes/Auth/page.tsx"),
+  route("/api/auth/*", "routes/api.auth.ts"),
 ] satisfies RouteConfig;
