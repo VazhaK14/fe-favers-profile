ced637dc849e2085860f4f462d6ca0523455f402
VazhaK14 <khayrivazhasocial@gmail.com>
2026-04-02 20:15:22 +0700

    fix: remove api route

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,2 @@
 import { type RouteConfig, index } from "@react-router/dev/routes";
-import { route } from "@react-router/dev/routes";
-export default [
-  index("routes/Home/page.tsx"),
-  route("/api/*", "routes/api.$.tsx"),
-] satisfies RouteConfig;
+export default [index("routes/Home/page.tsx")] satisfies RouteConfig;
